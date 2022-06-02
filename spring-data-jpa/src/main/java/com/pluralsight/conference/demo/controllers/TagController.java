10a2690745841beb4c652e29067f3c1f09c69069
anhvocap <anh.vo@capgemini.com>
2022-06-02 09:37:39 +0700

    - add hibernate config

--- spring-data-jpa/src/main/java/com/pluralsight/conference/demo/controllers/TagController.java
+++ spring-data-jpa/src/main/java/com/pluralsight/conference/demo/controllers/TagController.java
@@ -1,7 +1,35 @@
 package com.pluralsight.conference.demo.controllers;
 
-import org.springframework.stereotype.Controller;
+import com.pluralsight.conference.demo.models.Tag;
+import com.pluralsight.conference.demo.repositories.ITagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/tag")
 public class TagController {
+    @Autowired
+    private ITagRepository tagRepository;
+
+    @GetMapping
+    public List<Tag> list(@RequestParam(required = false) String name) {
+        if (name != null) {
+            return tagRepository.findByDescriptionContains(name);
+        } else {
+            return tagRepository.findAll();
+        }
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Tag get(@PathVariable Long id) {
+        return tagRepository.getById(id);
+    }
+
+    @PostMapping
+    public Tag create(@RequestBody final Tag tag){
+        return tagRepository.saveAndFlush(tag);
+    }
 }
